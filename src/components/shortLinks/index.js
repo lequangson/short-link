40a5057bc505c99504296d2bf01a7de18aa92fd8
myMac <lequangson1992@gmail.com>
2019-04-22 22:53:16 +0700

    the first commit

---
+++ src/components/shortLinks/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class shortLink extends Component {
+  render() {
+    return (
+      <div>
+        this page for shortlink
+      </div>
+    )
+  }
+}
